96cc7b49d0ec8af98c53f9c3a4096570dae62e2c
Hyper <hyper.jiang@lalamove.com>
2020-07-11 20:01:45 +0800

    add PV and FV

--- fomular_test.go
+++ fomular_test.go
@@ -13,9 +13,9 @@ func TestPMT(t *testing.T) {
 	periods := 12 * 30
 	amount := 1000000.00
 
-	should.Equal(-5307.267206228046, PMT(monthlyRate, periods, amount, 0, false))
-	should.Equal(-5285.683996575363, PMT(monthlyRate, periods, amount, 0, true))
-	should.Equal(-2777.777777777778, PMT(0, periods, amount, 0, false))
+	should.Equal(-5307.267206228046, PMT(monthlyRate, periods, amount, 0, 0))
+	should.Equal(-5285.683996575363, PMT(monthlyRate, periods, amount, 0, 1))
+	should.Equal(-2777.777777777778, PMT(0, periods, amount, 0, 0))
 }
 
 func TestIPMT(t *testing.T) {
@@ -25,8 +25,8 @@ func TestIPMT(t *testing.T) {
 	periods := 12 * 30
 	amount := 1000000.00
 
-	should.Equal(0.0, IPMT(monthlyRate, 0, periods, amount, 0, false))
-	should.Equal(-4083.333333333333, IPMT(monthlyRate, 1, periods, amount, 0, false))
+	should.Equal(0.0, IPMT(monthlyRate, 0, periods, amount, 0, 0))
+	should.Equal(-4083.333333333333, IPMT(monthlyRate, 1, periods, amount, 0, 0))
 }
 
 func TestPPMT(t *testing.T) {
@@ -36,6 +36,26 @@ func TestPPMT(t *testing.T) {
 	periods := 12 * 30
 	amount := 1000000.00
 
-	should.Equal(0.0, PPMT(monthlyRate, 0, periods, amount, 0, false))
-	should.Equal(-1223.9338728947132, PPMT(monthlyRate, 1, periods, amount, 0, false))
+	should.Equal(0.0, PPMT(monthlyRate, 0, periods, amount, 0, 0))
+	should.Equal(-1223.9338728947132, PPMT(monthlyRate, 1, periods, amount, 0, 0))
+}
+
+func TestPV(t *testing.T) {
+	should := require.New(t)
+
+	monthlyRate := 4.9 / 100 / 12
+	periods := 12 * 30
+	pmt := 1000.00
+
+	should.Equal(-188420.8880281185, PV(monthlyRate, periods, pmt, 0, 0))
+}
+
+func TestFV(t *testing.T) {
+	should := require.New(t)
+
+	monthlyRate := 4.9 / 100 / 12
+	periods := 12 * 30
+	pmt := 1000.00
+
+	should.Equal(-817037.6048461755, FV(monthlyRate, periods, pmt, 0, 0))
 }
--- formula.go
+++ formula.go
@@ -5,16 +5,16 @@ import "math"
 // PMT calculates the payment for a loan based on constant payments
 // and a constant interest rate.
 //
-// rate: is the interest rate for the loan. (monthly rate)
-// nper: is the total number of payments for the loan.
-// pv: is the present value, or the total amount that a series of future payments
+// rate is the interest rate for the loan. (monthly rate)
+// nper is the total number of payments for the loan.
+// pv is the present value, or the total amount that a series of future payments
 //  is worth now; also known as the principal.
-// fv: is the future value, or a cash balance you want to attain after the last
+// fv is the future value, or a cash balance you want to attain after the last
 //  payment is made. If fv is omitted, it is assumed to be 0 (zero), that is,
 //  the future value of a loan is 0.
-// beginning: adjust the payment to the beginning or end of the period.
-//  false, At the end of the period
-//  true, At the beginning of the period
+// flag adjust the payment to the beginning or end of the period.
+// 	0, At the end of the period
+//  1, At the beginning of the period
 //
 // If rate = 0:
 //        -(FV + PV)
@@ -28,42 +28,97 @@ import "math"
 // PMT = --------------------------------------------
 //                             /             nper \
 //                            | 1 - (1 + rate)     |
-//        (1 + rate * type) * | ------------------ |
+//        (1 + rate * flag) * | ------------------ |
 //                             \        rate      /
 //
-func PMT(rate float64, nper int, pv float64, fv float64, beginning bool) float64 {
+func PMT(rate float64, nper int, pv float64, fv float64, flag float64) float64 {
 	pvif := pvif(rate, nper)
 	fvifa := fvifa(rate, nper)
 
-	var f float64
-	if beginning {
-		f = 1
-	}
-
-	return (-pv*pvif - fv) / ((1.0 + rate*f) * fvifa)
+	return -(pv*pvif + fv) / ((1.0 + rate*flag) * fvifa)
 }
 
 // IPMT returns the interest payment for a given period for an investment based
 // on periodic, constant payments and a constant interest rate.
-func IPMT(rate float64, per int, nper int, pv float64, fv float64, beginning bool) float64 {
+func IPMT(rate float64, per int, nper int, pv float64, fv float64, flag float64) float64 {
 	if (per < 1) || (per >= (nper + 1)) {
 		return 0
 	}
 
-	pmt := PMT(rate, nper, pv, fv, beginning)
+	pmt := PMT(rate, nper, pv, fv, flag)
 	return calculateInterest(pv, pmt, rate, per-1)
 }
 
 // PPMT returns the payment on the principal for a given period for an investment
 // based on periodic, constant payments and a constant interest rate.
-func PPMT(rate float64, per int, nper int, pv float64, fv float64, beginning bool) float64 {
+func PPMT(rate float64, per int, nper int, pv float64, fv float64, flag float64) float64 {
 	if (per < 1) || (per >= (nper + 1)) {
 		return 0
 	}
 
-	pmt := PMT(rate, nper, pv, fv, beginning)
+	pmt := PMT(rate, nper, pv, fv, flag)
 	ipmt := calculateInterest(pv, pmt, rate, per-1)
 	return pmt - ipmt
+}
+
+// PV returns the present value of an investment. The present value is
+// the total amount that a series of future payments is worth now.
+// For example, when you borrow money, the loan amount is the present
+// value to the lender.
+//
+// If rate = 0:
+// PV = -(FV + PMT * nper)
+//
+// Else
+//                                 /              nper \
+//                                 | 1 - (1 + rate)    |
+//       PMT * (1 + rate * flag) * | ----------------- | - FV
+//                                 \        rate       /
+// PV = ------------------------------------------------------
+//                                nper
+//                       (1 + rate)
+//
+// rate is the interest rate per period. (monthly rate)
+// nper is the total number of payment periods in an annuity.
+// pmt  is the payment made each period and cannot change over the life of the annuity.
+// fv   is the future value, or a cash balance you want to attain after the last payment is made.
+// flag adjust the payment to the beginning or end of the period.
+//  0, At the end of the period
+//  1, At the beginning of the period
+func PV(rate float64, nper int, pmt float64, fv float64, flag float64) float64 {
+	pvif := pvif(rate, nper)
+	fvifa := fvifa(rate, nper)
+
+	return (-pmt*(1.0+rate*flag)*fvifa - fv) / pvif
+}
+
+// FV returns the future value of an investment based on periodic,
+// constant payments and a constant interest rate.
+//
+// For a more complete description of the arguments in FV, see the PV function.
+//
+// rate is the interest rate per period. (monthly rate)
+// nper is the total number of payment periods in an annuity.
+// pmt  is the payment made each period and cannot change over the life of the annuity.
+// pv   is the present value, or the lump-sum amount that a series of future payments is worth right now.
+// flag adjust the payment to the beginning or end of the period.
+//  0, At the end of the period
+//  1, At the beginning of the period
+//
+// If rate = 0:
+// FV = -(PV + PMT * nper)
+//
+// Else
+//                                 /             nper \
+//                                 | 1 - (1 + rate)     |                 nper
+// FV =  PMT * (1 + rate * flag) * | ------------------ | - PV * (1 + rate)
+//                                 \        rate      /
+//
+func FV(rate float64, nper int, pmt float64, pv float64, flag float64) float64 {
+	pvif := pvif(rate, nper)
+	fvifa := fvifa(rate, nper)
+
+	return -pmt*(1.0+rate*flag)*fvifa - pv*pvif
 }
 
 // Present value interest factor
